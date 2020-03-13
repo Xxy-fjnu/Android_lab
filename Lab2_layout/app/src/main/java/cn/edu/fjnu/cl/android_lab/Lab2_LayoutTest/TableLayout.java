fdfa6299a2a49ecf912e6c17b894c344d74b37fc
Xxy-fjnu <1808930960@qq.com>
2020-03-13 11:53:20 +0800

    Android

---
+++ Lab2_layout/app/src/main/java/cn/edu/fjnu/cl/android_lab/Lab2_LayoutTest/TableLayout.java
@@ -0,0 +1,18 @@
+package cn.edu.fjnu.cl.android_lab.Lab2_LayoutTest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import cn.edu.fjnu.cl.android_lab.R;
+
+/**
+ * Created by Lenovo on 2020-03-12.
+ */
+
+public class TableLayout extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tablelayout);
+    }
+}
